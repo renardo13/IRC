907842703ca78dc3276b64d3799457e49f7fcb38
Melanie Martin <melmarti@paul-f4Br6s1.clusters.42paris.fr>
2025-02-13 14:36:52 +0100

    before push

---
+++ server/objs/Channel.d
@@ -0,0 +1,20 @@
+objs/Channel.o: Srcs/Channel.cpp Srcs/../Includes/Channel.hpp \
+  Includes/../Includes/Client.hpp Includes/error.hpp Includes/Server.hpp \
+  Includes/Client.hpp Includes/Channel.hpp Includes/Command.hpp \
+  Includes/colors.hpp
+
+Srcs/../Includes/Channel.hpp:
+
+Includes/../Includes/Client.hpp:
+
+Includes/error.hpp:
+
+Includes/Server.hpp:
+
+Includes/Client.hpp:
+
+Includes/Channel.hpp:
+
+Includes/Command.hpp:
+
+Includes/colors.hpp:
---
+++ server/objs/handle_commands.d
@@ -0,0 +1,26 @@
+objs/handle_commands.o: Srcs/handle_commands.cpp \
+  Srcs/../Includes/Client.hpp Includes/error.hpp Includes/Server.hpp \
+  Includes/Client.hpp Includes/Channel.hpp \
+  Includes/../Includes/Client.hpp Includes/Command.hpp \
+  Includes/colors.hpp Srcs/../Includes/Server.hpp \
+  Srcs/../Includes/replies.hpp
+
+Srcs/../Includes/Client.hpp:
+
+Includes/error.hpp:
+
+Includes/Server.hpp:
+
+Includes/Client.hpp:
+
+Includes/Channel.hpp:
+
+Includes/../Includes/Client.hpp:
+
+Includes/Command.hpp:
+
+Includes/colors.hpp:
+
+Srcs/../Includes/Server.hpp:
+
+Srcs/../Includes/replies.hpp:
